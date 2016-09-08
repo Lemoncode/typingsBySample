6e00b8df0e1d7574a044b756b673a1d7143555c1
Braulio <braulio.diez@gmail.com>
2016-09-08 17:11:51 +0100

    no message

---
+++ 00 Start/readme.md
@@ -0,0 +1,26 @@
+# Sample 00: Start
+
+This is the starting point, this sample is a refactored version of
+"02 Fx / 00 Typescript" that can be found in the [webpack-1.x-by-sample
+repository](https://github.com/Lemoncode/webpack-1.x-by-sample/tree/master/02%20Fx/00%20Typescript)
+
+# About the sample:
+
+- It's a simple application that displays a console log.
+- Tool / Bundler to build and launch the project webpack.
+
+# Steps to run the sample
+
+First time when you download the sample do not forget to install the dependencies,
+open the command prompt or bash navigate to the project 00 Start folder and
+execute:
+
+````
+npm install
+````
+
+Now to run the sample, run the following command from the command prompt:
+
+````
+npm start
+````
---
+++ 00 Start/webpack.config.js
@@ -0,0 +1,52 @@
+var path = require("path");
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+var basePath = __dirname;
+
+module.exports = {
+	resolve: {
+	      extensions: ['', '.js', '.ts']
+	},
+	entry: ["./main.ts"],
+	output: {
+		path: path.join(basePath, "dist"),
+		filename: "bundle.js"
+	},
+
+	//https://webpack.github.io/docs/webpack-dev-server.html#webpack-dev-server-cli
+  devServer: {
+    contentBase: './dist', //Content base
+    inline: true, //Enable watch and live reload
+    host: 'localhost',
+    noInfo: true,
+    port: 8080
+  },
+
+	devtool: 'source-map',
+
+	module: {
+		loaders: [
+			{
+	      test: /\.(ts)$/,
+	      exclude: /node_modules/,
+	      loader: 'ts-loader'
+      },
+			{
+				test: /\.js$/,
+        loader: "babel-loader",
+				exclude: /node_modules/,
+        query: {
+          presets: ['es2015']
+        }
+			}
+		]
+	},
+	plugins:[
+    //Generate index.html in /dist => https://github.com/ampedandwired/html-webpack-plugin
+    new HtmlWebpackPlugin({
+      filename: 'index.html', //Name of file in ./dist/
+      template: 'index.html', //Name of template in ./src
+			hash: true
+    })
+  ]
+}
